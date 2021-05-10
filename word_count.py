f40077fd3f97ea25cf4f008cae14c525317b32d0
acolbert1 <acolbert1@gmail.com>
2021-05-09 17:58:58 -0700

    refactoring

--- word_count.py
+++ word_count.py
@@ -1,19 +1,40 @@
-#ask the user for input
-#count each word in the input until there is nothing left to count. search google for: if there is a space continue until the next letter then resume counting?
-#print the number of words in the 
+import os.path
+from os import path
 
-
-
-def word_count_input():
+#this method is for parsing through a file in the same folder.
+def file_count_input():
     count = 0
-    user_input = input("What's on your mind today? ")
-    for i in user_input:
-        if i == " ":
-            count -= 1
-        count += 1
+    if path.exists('tesssdsdt.txt') == True:
+        with open('test.txt', 'r')  as f:
+            data = f.read()
+            for i in data:
+                if i == " ":
+                    count -= 1
+                count += 1
+    else:
+        user_input = input("What's on your mind today? ")
+        for i in user_input:
+            if i == " ":
+                count -= 1
+            count += 1
     print("oh nice, you just told me what's on your mind in " + str(count) + " words!")
+            
+
+
+#this method is for parsing through user input.
+# def word_count_input():
+#     count = 0
+#     user_input = input("What's on your mind today? ")
+#     for i in user_input:
+#         if i == " ":
+#             count -= 1
+#         count += 1
+#     print("oh nice, you just told me what's on your mind in " + str(count) + " words!")
 
     
 
 
-word_count_input()
+# word_count_input()
+file_count_input()
+
+#if data exists, user input = data otherwise user_input will run
